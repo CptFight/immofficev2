9face247eb7f94b9eca9260c509f53a93ddd5f9f
Pirson Gabygaël <gabygael@infine-digital.be>
2017-03-21 11:18:28 +0100

    aa

--- application/views/common/messages.php
+++ application/views/common/messages.php
@@ -1,6 +1,9 @@
-<?php $messages = $this->session->flashdata('messages');; ?>
+<?php $messages = $this->session->flashdata('messages');?>
+<?php $errors = $this->session->flashdata('errors');?>
 
-<?php if(count($messages) > 0 && count($errors) == 0) { ?>
+<?php if(!$errors || !is_array($errors)) $errors = array(); ?>
+
+<?php if($messages && is_array($messages) && count($messages) > 0 && count($errors) == 0) { ?>
 <div style="border:1px; color:green;">
 <ul class="messages">
 <?php foreach($messages as $key => $message){ ?>
