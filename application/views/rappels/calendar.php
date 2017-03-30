e9a3a1d699ebd02d312f64eda18f54965cca7ad4
meg <megpirson@gmail.com>
2017-03-30 17:01:24 +0200

    page calendar

--- application/views/rappels/calendar.php
+++ application/views/rappels/calendar.php
@@ -8,16 +8,18 @@
         </div>
     </div>
 
-     <div class='clearfix'>
-      	<div class="float-middle input-separation">
+     <div class='clearfix l-calendar'>
+      	<div class="float-middle input-separation" id="calendar_week_container">
             <fieldset class="inputstyle">
                 <div id='calendar_week'></div>
+                <a href="javascript:;" class="btn btn-calendar" data-id="calendar_week_container"><i class="fa fa-arrows-alt"></i></a>
             </fieldset>
         </div>
 
-        <div class="float-middle input-separation">
+        <div class="float-middle input-separation" id="calendar_month_container">
            <fieldset class="inputstyle">
                 <div id='calendar_month'></div>
+                 <a href="javascript:;" class="btn btn-calendar" data-id="calendar_month_container"><i class="fa fa-arrows-alt"></i></a>
             </fieldset>
         </div>
        
