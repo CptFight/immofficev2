6307a6cab1f605f2082c204f483b820ae8d41046
meg <megpirson@gmail.com>
2017-04-27 14:21:15 +0200

    affichage (tout sauf -> changement tags en mobile / IE / bug des dates telephone en mobile)

--- application/views/supervision/index.php
+++ application/views/supervision/index.php
@@ -1,5 +1,5 @@
 <section class="l-annonces-search l-annonces-section apparitionright">
-    <div class="l-annonces-form l-form">
+    <div class="l-annonces-form">
         <input type="hidden" name="agence_id" id="agence_id" value="<?php echo $current_user->agence_id; ?>" />
         <ul class="tabs">
             <li><div id="tab1" data-select="tab1" class='active'><?php echo $this->lang->line('dashboard'); ?></div></li>
